ea58e32382c52407230e16d9c232b5630caad063
Arjun-Mahto <arjunmahto139@gmail.com>
2025-08-27 14:28:06 +0530

    resourcesPage added

---
+++ resumeBuilder/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Check from './Components/Home/Check'
+
+function App() {
+  
+  return (
+    <>
+      <Check/>
+    </>
+  )
+}
+
+export default App
