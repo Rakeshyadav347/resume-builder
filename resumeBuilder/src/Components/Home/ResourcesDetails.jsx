3796e50d11b67eb0889c5a1bbb8baac937e9b45d
Arjun-Mahto <arjunmahto139@gmail.com>
2025-08-28 14:31:59 +0530

    added the resources details page

---
+++ resumeBuilder/src/Components/Home/ResourcesDetails.jsx
@@ -0,0 +1,49 @@
+
+
+function ResouceDetails() {
+    return (
+        <>
+       <div className="flex gap-2 px-4 md:px-20 py-10 h-auto w-full flex-col md:flex-row md:gap-20">
+
+            <div className="hidden  md:bg-gray-200 md:flex md:flex-col md:gap-3 md:h-100 md:w-60">    <span>Contacts</span>
+                        <hr className="w-full"/>
+                    <p className="">Personal Info</p>
+                    <p>Personal Info</p>
+                    <p>Personal Info</p>
+                    <p>Personal Info</p>
+                   
+            
+            </div>
+
+        <div className="w-full ml-0 md:w-250">
+            <span><img className=" w-full md:w-full" src="\ResourcesDetail_images\Frame 1000004069.png"></img></span>
+            <span className="flex flex-col gap-3">
+
+            <p className="font-bold text-lg ">Functional Resume Template and Writing Tips</p>
+            <p>OpenAI doesn’t release new models gradually. Remember GPT-4 Silence, silence, then boom — the world changed overnight.</p>
+            <p>In February 2024, Sam Altman said GPT-5 would follow 4.5 “in months, not years.” If you do the math, that puts us squarely in Summer 2025. And the chatter inside OpenAI supports that timeline.</p>
+            <p>So what can we expect</p>
+            </span>
+            <span>
+                <img className="md:w-250 md:mt-16" src="\ResourcesDetail_images\image 7.png"></img>
+            </span>
+            <span className="flex flex-col gap-3 md:mt-8">
+                 <p className="font-bold text-lg ">Functional Resume Template and Writing Tips</p>
+            <p>OpenAI doesn’t release new models gradually. Remember GPT-4 Silence, silence, then boom — the world changed overnight.</p>
+            <p>In February 2024, Sam Altman said GPT-5 would follow 4.5 “in months, not years.” If you do the math, that puts us squarely in Summer 2025. And the chatter inside OpenAI supports that timeline.</p>
+            <p>So what can we expect</p>
+            <p>OpenAI doesn’t release new models gradually. Remember GPT-4 Silence, silence, then boom — the world changed overnight.</p>
+            <p>In February 2024, Sam Altman said GPT-5 would follow 4.5 “in months, not years.” If you do the math, that puts us squarely in Summer 2025. And the chatter inside OpenAI supports that timeline.OpenAI doesn’t release new models gradually. Remember GPT-4 Silence, silence, then boom — the world changed overnight.</p>
+            <p>In February 2024, Sam Altman said GPT-5 would follow 4.5 “in months, not years.” If you do the math, that puts us squarely in Summer 2025. And the chatter inside OpenAI supports that timeline.OpenAI doesn’t release new models gradually. Remember GPT-4 Silence, silence, then boom — the world changed overnight.</p>
+            <p>In February 2024, Sam Altman said GPT-5 would follow 4.5 “in months, not years.” If you do the math, that puts us squarely in Summer 2025. And the chatter inside OpenAI supports that timeline.OpenAI doesn’t release new models gradually. Remember GPT-4 Silence, silence, then boom — the world changed overnight.</p>
+            <p>In February 2024, Sam Altman said GPT-5 would follow 4.5 “in months, not years.” If you do the math, that puts us squarely in Summer 2025. And the chatter inside OpenAI supports that timeline.OpenAI doesn’t release new models gradually. Remember GPT-4 Silence, silence, then boom — the world changed overnight.</p>
+
+            </span>
+        </div>
+        </div>
+        
+        </>
+    )
+}
+
+export default ResouceDetails;
