b1e40d621d7f5a80bafeaaac95c116064401097d
Rakeshyadav347 <rk7897624@gmail.com>
2025-09-11 14:02:18 +0530

    this is deploy commit

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,8 @@ app.use(express.json());
 app.use(cors({
   origin:process.env.FRONTEND_URL,
   credentials:true,
-  methods:['GET', 'POST' , 'PUT' ,'DELETE' ,'OPTIONS'],
-  allowedHeaders:['Content_Type', 'Authoriztion']
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
 app.use("/api/adminauth",adminLogRouter);
